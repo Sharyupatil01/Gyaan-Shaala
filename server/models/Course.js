7a173794259f0f66960c9799725534b81e30469d
ashlesha2705 <ashleshasangle27@gmail.com>
2025-08-08 13:48:02 +0530

    Added new models

---
+++ server/models/Course.js
@@ -0,0 +1,53 @@
+const mongoose=require("mongoose")
+
+const courseSchema=new mongoose.Schema({
+    courseName:{
+        type:String,
+        required:true,
+    },
+    courseDescription:{
+        type:String,
+        required:true
+    },
+    courseDuration:{
+        type:String,
+        required:true
+    },
+    coursePrice:{
+        type:Number,
+        required:true
+    },
+    Instructor:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    },
+    Whatyouwilllearn:{
+        type:String,
+        required:true
+    },
+    RatingandReviews:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"RatingandReview"
+        }
+    ],
+    courseImage:{
+        type:String,
+        required:true
+    },
+    courseContent:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Section"
+    },
+    Tag:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Tag"
+    },
+    studentsEnrolled:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    }]
+})
+
+module.exports=mongoose.model("Course",courseSchema)
